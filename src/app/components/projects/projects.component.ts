c82b4984e4ce0f531c824d2eeddd67ce00fd075b
Zach Poole <zachpoolework@gmail.com>
2020-05-18 10:54:53 -0500

    messing with styling on projects

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -12,22 +12,25 @@ export class ProjectsComponent implements OnInit {
       title: 'Personal Website',
       desc: 'This is the website you are currently on!',
       technologies: ['Angular', 'Bootstrap 4'],
-      github: 'leaguestats',
-      color: 'pastel-red'
+      github: 'personalwebsite',
+      color: 'pastel-red',
+      finished: true
     },
     {
       title: 'Dartboard Score Tracker',
       desc: 'Me and my roommate loved playing darts, but hated keeping score on pieces of paper, throwing them away, and then forgetting who had more wins over the other. I made this app to help keep track of the score and historical game data. I also made this with the intention of it eventually going live so that anyone out there can use it for free.In the future I plan on adding login functionality and a data dashboard for each profile. ',
       technologies: ['React', 'Styled Components'],
-      github: 'Interview',
-      color: 'pastel-blue'
+      github: 'dartboardscoreapp',
+      color: 'pastel-blue',
+      finished: false
     },
     {
       title: 'Nothin But Net (NBN)',
       desc: "A friend of mine has a passion is basketball and is seeking a career in it. This website is a place where he can unload all of his views on the sport and a place to make team information easily accessbile to him and others.",
       technologies: ['Vue', 'VueX'],
-      github: 'Interview',
-      color: 'pastel-green'
+      github: 'nbn-app',
+      color: 'pastel-green',
+      finished: false
     }
   ]
 
