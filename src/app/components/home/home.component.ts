c6c3f7b4903544e57b760f9d4a4bd3797935377f
zach.poole <zach.poole@sustainment.tech>
2021-11-08 12:59:45 -0600

    testing GA custom event

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+declare let gtag: Function;
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
 })
 export class HomeComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  sendAnalyticsEvent(): void {
+    //prettier-ignore
+    gtag('event', 'user_login', {
+      'event_category': 'General Site Data',
+      'event_label': 'Successful User Login',
+      'value': 1,
+    });
   }
-
 }
